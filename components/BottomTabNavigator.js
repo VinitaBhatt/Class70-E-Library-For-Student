0c8a37ff9bdd31b8109f59516637c2f3aababc79
VinitaBhatt <71602249+VinitaBhatt@users.noreply.github.com>
2022-03-02 14:37:33 +0530

    Add files via upload

--- components/BottomTabNavigator.js
+++ components/BottomTabNavigator.js
@@ -26,7 +26,27 @@ export default class BottomTabNavigator extends Component {
               <Ionicons name={icon} size={size} color={color}/>
             )
           }
-        })}>
+        })}
+        
+        tabBarOptions={{
+          activeTintColor:"white",
+          inactiveTintColor:"grey",
+          labelStyle:{
+            fontSize:15,
+            fontFamily:'Rajdhani_600SemiBold'
+          },
+          labelPosition:"beside-icon",
+          tabStyle:{
+            marginTop:15,
+            marginLeft:10,
+            marginRight:10,
+            borderRadius:30,
+            backgroundColor:"#5653D4",
+            borderWidth:2,
+            alignItems:"center",
+            justifyContent:"center"
+          }
+        }}>
           <Tab.Screen name="Transaction" component={TransactionScreen} />
           <Tab.Screen name="Search" component={SearchScreen} />
         </Tab.Navigator>
